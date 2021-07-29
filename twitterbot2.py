e7bd0162ff84042883b040395fd5f4a4a49759bc
edoardottt <edoardott@gmail.com>
2021-07-29 12:43:09 +0200

    update

--- twitterbot2.py
+++ twitterbot2.py
@@ -8,4 +8,40 @@
 #   This repository is under GPL-3 License.
 #
 
-from twitter import *
+import twitter
+
+
+def auth(token, token_secret, consumer_key, consumer_secret):
+    """
+    This function provides the authentication to twitter.
+    """
+    t = twitter.Twitter(
+        auth=twitter.OAuth(token, token_secret, consumer_key, consumer_secret)
+    )
+    return t
+
+
+def get_home(t):
+    # Get your "home" timeline
+    return t.statuses.home_timeline()
+
+
+def get_friend_home(t, name):
+    # Get a particular friend's timeline
+    t.statuses.user_timeline(screen_name=name)
+
+
+def tweet(t, message):
+    # Update your status
+    t.statuses.update(status=message)
+
+
+def put_like(t, status):
+    # Favorite/like a status
+    if not status["favorited"]:
+        t.favorites.create(_id=status["id"])
+
+
+def search(t, term):
+    # Search for the latest tweets about <term>
+    return t.search.tweets(q=term)
