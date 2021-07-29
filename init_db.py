a5e31ed530f20a67cd9ae34aa1d289db1e6b8758
edoardottt <edoardott@gmail.com>
2021-07-29 12:47:27 +0200

    update

---
+++ init_db.py
@@ -0,0 +1,30 @@
+import os
+import sqlite3
+
+db_filename = "database.db"
+
+db_is_new = not os.path.exists(db_filename)
+
+conn = sqlite3.connect(db_filename)  # connect to the database or create it
+
+
+# ctreate table with the sql code input
+def create_table(conn, create_table_sql):
+    try:
+        c = conn.cursor()
+        c.execute(create_table_sql)
+    except Exception as e:
+        print(e)
+
+
+sql_create_analytics_table = """CREATE TABLE IF NOT EXISTS analytics (
+                                    username text NOT NULL,
+                                    date date NOT NULL,
+                                    likes integer NOT NULL,
+                                    retweets integer NOT NULL,
+                                    followers integer NOT NULL,
+                                    PRIMARY KEY (username,date)
+                                );"""
+if conn is not None:
+    create_table(conn, sql_create_analytics_table)
+conn.close()
