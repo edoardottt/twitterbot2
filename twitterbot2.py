7797feb77b4135a4244dd83e4b787922d6deea85
edoardottt <edoardott@gmail.com>
2021-08-21 18:31:09 +0200

    update logging

--- twitterbot2.py
+++ twitterbot2.py
@@ -177,7 +177,7 @@ def main():
     """
 
     logging.basicConfig(
-        encoding="utf-8", level=logging.DEBUG, format="%(asctime)s %(message)s"
+        encoding="utf-8", level=logging.DEBUG, format="%(asctime)s | %(message)s"
     )
 
     args = input.get_args()
