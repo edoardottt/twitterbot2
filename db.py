f6ee3c380886acb9a8e8685ea30f53a5cd59e674
edoardottt <edoardott@gmail.com>
2021-08-21 16:35:53 +0200

    Add RetrieveProtocol function useful for #36

--- db.py
+++ db.py
@@ -8,12 +8,31 @@
 #   This repository is under GPL-3 License.
 #
 
+# TABLE statistics:
+#
+# username text NOT NULL,
+# date date NOT NULL,
+# likes integer NOT NULL,
+# retweets integer NOT NULL,
+# followers integer NOT NULL,
+# PRIMARY KEY (username, date)
+
 
 def create_stat(conn, data):
     """
     This function creates a record in the statistics table.
     """
     sql = """ INSERT INTO statistics VALUES(?,?,?,?,?) """
+    cur = conn.cursor()
+    cur.execute(sql, data)
+    conn.commit()
+
+
+def update_stat(conn, data):
+    """
+    This function updates the record of today with new up to date values.
+    """
+    sql = """ UPDATE statistics SET VALUES(?,?,?,?,?) WHERE ? """
     cur = conn.cursor()
     cur.execute(sql, data)
     conn.commit()
