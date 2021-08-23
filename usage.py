96957bd9ccb7c64066502ffebc242a35a74bbd53
edoardottt <edoardott@gmail.com>
2021-08-23 17:34:31 +0200

    update output

--- usage.py
+++ usage.py
@@ -11,7 +11,7 @@
 
 def usage():
     """
-    usage: twitterbot2.py [-h] [-v | -t | -k KEYWORD | -s]
+    usage: twitterbot2.py [-h] [-v | -t | -k KEYWORD | -s STATS | -oc OUTPUT_CSV | -oj OUTPUT_JSON | -oh OUTPUT_HTML]
 
     Twitterbot v2
 
@@ -22,7 +22,13 @@ def usage():
       -k KEYWORD, --keyword KEYWORD
                             Search for tweets with a defined keyword.
       -s STATS, --stats STATS
-                        Show the statistics of the inputted bot (username).
+                            Show the statistics of the inputted bot (username).
+      -oc OUTPUT_CSV, --output-csv OUTPUT_CSV
+                            Produce a csv file containing the stats for the inputted used (ALL for anyone).
+      -oj OUTPUT_JSON, --output-json OUTPUT_JSON
+                            Produce a json file containing the stats for the inputted used (ALL for anyone).
+      -oh OUTPUT_HTML, --output-html OUTPUT_HTML
+                            Produce a html file containing the stats for the inputted used (ALL for anyone).
     """
     print("usage: twitterbot2.py [-h] [-v | -t | -k KEYWORD | -s]")
     print("")
@@ -33,6 +39,21 @@ def usage():
     print("  -v, --version  Show the version of this program.")
     print("  -t, --timeline  Search for tweets in the bot and user's timeline.")
     print("  -k KEYWORD, --keyword KEYWORD")
-    print("                      Search for tweets with a defined keyword.")
+    print("                        Search for tweets with a defined keyword.")
     print("  -s STATS, --stats STATS")
-    print("                  Show the statistics of the inputted bot (username).")
+    print("                        Show the statistics of the inputted bot (username).")
+    print("  -oc OUTPUT_CSV, --output-csv OUTPUT_CSV")
+    print(
+        "                        Produce a csv file containing the stats for the inputted used (ALL for anyone)."
+    )
+    print("  -oj OUTPUT_JSON, --output-json OUTPUT_JSON")
+    print(
+        "                        Produce a json file containing the stats for the inputted used (ALL for anyone)."
+    )
+    print("  -oh OUTPUT_HTML, --output-html OUTPUT_HTML")
+    print(
+        "                        Produce a html file containing the stats for the inputted used (ALL for anyone)."
+    )
+
+
+usage()
