22fd9cf188840a27abbb61469df7e9af7e4f5c17
edoardottt <edoardott@gmail.com>
2021-08-26 14:52:31 +0200

    globals.py header

--- globals.py
+++ globals.py
@@ -7,6 +7,10 @@
 #
 #   This repository is under GPL-3 License.
 #
+# This file contains some global values needed by
+# twitterbot2 while running.
+# You should edit the last two ('user' and 'bot_user').
+#
 
 month_tweet_cap = 500000
 daily_tweet_cap = 2000
