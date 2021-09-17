bceb84c3f3232eeb8ff464f517662aa790dbaf4d
gilfoyle97 <edoardott@gmail.com>
2021-09-17 18:06:30 +0200

    Update update-devel.sh

--- shutils/update-devel.sh
+++ shutils/update-devel.sh
@@ -2,7 +2,7 @@ cp database.db ..
 cp config.yaml ..
 cd ..
 rm -rf twitterbot2
-git clone --single-branch -branch devel https://github.com/edoardottt/twitterbot2
+git clone --single-branch --branch devel https://github.com/edoardottt/twitterbot2
 cd twitterbot2
 cp ../database.db .
 cp ../config.yaml .
