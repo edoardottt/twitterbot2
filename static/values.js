8f8034689826a172306890d7caa4713f030754aa
edoardottt <edoardott@gmail.com>
2021-09-18 11:57:23 +0200

    update chart

--- static/values.js
+++ static/values.js
@@ -1,38 +1,70 @@
-//var values = JSON.parse(document.getElementById("myChart").dataset.values);
-//console.log(data);
+var values = JSON.parse(document.getElementById("myChart").dataset.values);
+
+var dates = values.slice(0, n);
+var tweets = values.slice(1, n);
+var likes = values.slice(2, n);
+var retweets = values.slice(3, n);
+var followers = values.slice(4, n);
+
+// NOT WORKING
 
 var ctx = document.getElementById('myChart').getContext('2d');
 var myChart = new Chart(ctx, {
-    type: 'bar',
+    type: 'line',
     data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            y: {
-                beginAtZero: true
+        labels: dates,
+        datasets: [
+            {
+                label: 'Tweets',
+                data: tweets,
+                borderColor: Utils.CHART_COLORS.red,
+                backgroundColor: Utils.transparentize(Utils.CHART_COLORS.red, 0.5),
+                yAxisID: 'y',
+            },
+            {
+                label: 'Likes',
+                data: likes,
+                borderColor: Utils.CHART_COLORS.blue,
+                backgroundColor: Utils.transparentize(Utils.CHART_COLORS.blue, 0.5),
+                yAxisID: 'y1',
+            },
+            {
+                label: 'Retweets',
+                data: retweets,
+                borderColor: Utils.CHART_COLORS.yellow,
+                backgroundColor: Utils.transparentize(Utils.CHART_COLORS.red, 0.5),
+                yAxisID: 'y',
+            },
+            {
+                label: 'Followers',
+                data: followers,
+                borderColor: Utils.CHART_COLORS.green,
+                backgroundColor: Utils.transparentize(Utils.CHART_COLORS.blue, 0.5),
+                yAxisID: 'y1',
             }
+        ]
+    },
+    plugins: {
+        title: {
+            display: true,
+            text: 'Statistics over time'
         }
+    },
+    scales: {
+        y: {
+            type: 'linear',
+            display: true,
+            position: 'left',
+        },
+        y1: {
+            type: 'linear',
+            display: true,
+            position: 'right',
+
+            // grid line settings
+            grid: {
+                drawOnChartArea: false, // only want the grid lines for one axis to show up
+            },
+        },
     }
 });
