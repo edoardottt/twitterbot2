f83c2bbaddb25c17e4393067eb87ea468b7b164c
edoardottt <edoardott@gmail.com>
2021-09-27 17:26:49 +0200

    twitter errors handling #11

--- twitterbot2.py
+++ twitterbot2.py
@@ -246,7 +246,7 @@ def crawl_timeline(bot, logger):
             logger.info("Database updated.")
             logger.error(str(e.e) + " on " + e.uri)
             logger.info("Sleeping for 15 minutes.")
-            time.sleep(15 * 60)
+            time.sleep(60 * 60)
 
 
 def likes_rt_search(bot, logger, keyword, tweet_count, likes_count, retweet_count):
@@ -384,7 +384,7 @@ def crawl_keyword(bot, logger, keyword):
             logger.info("Database updated.")
             logger.error(str(e.e) + " on " + e.uri)
             logger.info("Sleeping for 15 minutes.")
-            time.sleep(15 * 60)
+            time.sleep(60 * 60)
 
 
 def main():
