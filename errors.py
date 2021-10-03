6ef7ce5b26a6b2d22fc8bb7d5c5d024cc5d4bd92
edoardottt <edoardott@gmail.com>
2021-10-03 12:06:04 +0200

    Errors handling improvement & formatting

---
+++ errors.py
@@ -0,0 +1,38 @@
+#
+# twitterbot2
+#
+#   edoardottt
+#   edoardoottavianelli.it
+#   https://github.com/edoardottt/twitterbot2
+#
+#   This repository is under GPL-3 License.
+#
+#
+# This file contains the logic when a new TwitterHTTPError
+# happens.
+#
+
+import time
+import logging
+
+
+def error_handler(e):
+    """
+    This function handles all the TwitterHTTPError errors
+    """
+    logger = logging.getLogger("__main__")
+    logger.error(str(e.e) + " on " + e.uri)
+
+    # == 429 TOO MANY REQUESTS -> Sleep for one hour
+    if str(e.code) == "429":
+        logger.info("Sleeping for one hour.")
+        time.sleep(60 * 60)
+        return
+
+    # == 403 FORBIDDEN -> Sleep for ten seconds
+    if str(e.code) == "403":
+        logger.info("Sleeping for ten seconds.")
+        time.sleep(10)
+        return
+
+    time.sleep(10)
