ec5a80aea7e964bbdb47c7da0516b1a2de100762
edoardottt <edoardott@gmail.com>
2021-10-08 15:53:47 +0200

    Add no user option

--- usage.py
+++ usage.py
@@ -14,7 +14,7 @@
 
 def usage():
     """
-    usage: twitterbot2.py [-h] [-v | -t | -k KEYWORD | -s STATS | -oc OUTPUT_CSV | -oj OUTPUT_JSON | -oh OUTPUT_HTML]
+    usage: twitterbot2.py [-h] [-v | -t | -k KEYWORD | -nu | -s STATS | -oc OUTPUT_CSV | -oj OUTPUT_JSON | -oh OUTPUT_HTML]
 
     Twitterbot v2
 
@@ -24,6 +24,7 @@ def usage():
       -t, --timeline        Search for tweets in the bot and user's timeline.
       -k KEYWORD, --keyword KEYWORD
                             Search for tweets with a defined keyword.
+      -nu, --no-user        Don't like and retweet user tweets.
       -s STATS, --stats STATS
                             Show the statistics of the inputted bot (username).
       -oc OUTPUT_CSV, --output-csv OUTPUT_CSV
@@ -43,6 +44,7 @@ def usage():
     print("  -t, --timeline  Search for tweets in the bot and user's timeline.")
     print("  -k KEYWORD, --keyword KEYWORD")
     print("                        Search for tweets with a defined keyword.")
+    print("  -nu, --no-user Don't like and retweet user tweets.")
     print("  -s STATS, --stats STATS")
     print("                        Show the statistics of the inputted bot (username).")
     print("  -oc OUTPUT_CSV, --output-csv OUTPUT_CSV")
