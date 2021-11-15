e4c55e7957dc883e86f5f41e4da480674a9a9e2f
edoardottt <edoardott@gmail.com>
2021-11-15 12:40:45 +0100

    Update usage

--- usage.py
+++ usage.py
@@ -23,8 +23,11 @@ def usage():
       -v, --version         Show the version of this program.
       -t, --timeline        Search for tweets in the bot and user's timeline.
       -k KEYWORD, --keyword KEYWORD
-                            Search for tweets with defined keyword(s). If more than one, comma separated enclosed in double quotes.
+                            Search for tweets with defined keyword(s). If more than one,
+                            comma separated enclosed in double quotes.
       -nu, --no-user        Don't like and retweet user tweets.
+      -nl, --no-like        Don't like tweets, just retweet.
+      -nr, --no-retweet     Don't retweet tweets, just like.
       -s STATS, --stats STATS
                             Show the statistics of the inputted bot (username).
       -oc OUTPUT_CSV, --output-csv OUTPUT_CSV
@@ -43,8 +46,13 @@ def usage():
     print("  -v, --version  Show the version of this program.")
     print("  -t, --timeline  Search for tweets in the bot and user's timeline.")
     print("  -k KEYWORD, --keyword KEYWORD")
-    print("                        Search for tweets with a defined keyword.")
-    print("  -nu, --no-user Don't like and retweet user tweets.")
+    print(
+        "                        Search for tweets with defined keyword(s). If more than one,"
+    )
+    print("                        comma separated enclosed in double quotes.")
+    print("  -nu, --no-user     Don't like and retweet user tweets.")
+    print("  -nl, --no-like     Don't like tweets, just retweet.")
+    print("  -nr, --no-retweet  Don't retweet tweets, just like.")
     print("  -s STATS, --stats STATS")
     print("                        Show the statistics of the inputted bot (username).")
     print("  -oc OUTPUT_CSV, --output-csv OUTPUT_CSV")
