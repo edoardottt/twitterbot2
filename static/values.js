f711e4e3e8f1a981ad16815fe5a20dadedc377c8
edoardottt <edoardott@gmail.com>
2022-01-07 18:56:47 +0100

    update

--- static/values.js
+++ static/values.js
@@ -135,13 +135,13 @@ var myChart = new Chart(ctx, {
     scaleShowVerticalLines: false,
 });
 
-// on mobile
+// --- On mobile ---
 if (window.innerHeight > window.innerWidth) {
     (document.getElementsByClassName("chart-container")[0]).style.width = "100%";
     document.getElementById("formRange").value = 100;
     console.log(document.getElementById("formRange").value);
 } else {
-    // on desktop
+    // --- On desktop ---
     (document.getElementsByClassName("chart-container")[0]).style.width = "75%";
     document.getElementById("formRange").value = 75;
     console.log(document.getElementById("formRange").value);
